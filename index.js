5fd1fd86d05ea0c850409790cac0b96f05b55142
Kyllian <kyllianvibancos2@gmail.com>
2025-04-09 11:58:21 +0200

    redid html, create task done

--- index.js
+++ index.js
@@ -1,49 +1,79 @@
 let taskName = document.querySelector("#task-name");
-let taskDesc = document.querySelector("#task-desc");
-let submit = document.querySelector("button");
-let ul = document.querySelector("ul");
-let check = document.querySelectorAll("#check");
-// let newTask = document.querySelectorAll("newTask");
+let submit = document.querySelector("#submit");
+let taskList = document.querySelector("#task-list");
+
 let id = 0;
 let newTask;
+let taskItems = [];
+console.log(taskName);
 
-class Task {
-	constructor(id, name, description, isChecked) {
+//createelement appendchild pour ajouter
+// class TodoList {
+// 	constructor(id, title, taskItems) {
+// 		this.id = id++;
+// 		this.title = title;
+// 		this.taskItems = taskItems;
+// 	}
+// }
+class Todo {
+	constructor(id, title) {
 		this.id = id++;
-		this.name = name;
-		this.description = description;
-		this.isChecked = isChecked;
+		this.title = title;
+		this.completed = false;
+	}
+	removeTask(index) {
+		if (index >= 0 && index < this.taskItems.length) {
+			this.taskItems.splice(index, 1);
+		}
+	}
+	toggle() {
+		this.completed = !this.completed;
 	}
 }
 
 submit.addEventListener("click", (event) => {
-	event.preventDefault(); //éviter que le submit du form recharge la page
+	event.preventDefault();
 	try {
 		if (
 			taskName.value === "" ||
 			taskName.value === null ||
 			taskName.value === undefined
 		) {
-			alert("Invalid input");
+			alert("Please enter a title for your task");
 		} else {
-			newTask = new Task(
-				id++,
-				`${taskName.value}`,
-				`${taskDesc.value}`,
-				false
-			);
-			ul.innerHTML += `<li class="list-group-item d-flex justify-content-between align-items-center">
-            <input type="checkbox" id="check"/>
-            <h2>${newTask.name}</h2>
-            <p>${newTask.description}</p>
-            <button id="delete">Supprimer</button>
-            </li>`;
+
+			//Instanciation de la classe et ajout dans le tableau
+			newTask = new Todo(id++, `${taskName.value}`);
+			taskItems.push(newTask);
+			console.log(taskItems);
+
+			//Création d'éléments dynamiques
+			textNode = document.createTextNode(newTask.title);
+			newDiv = document.createElement("div");
+			newCheckBox = document.createElement("input");
+			newCheckBox.type = "checkbox";
+			newCheckBox.id = newTask.id;
+			newCheckBox.value = newTask.completed;
+
+			//Style des éléments
+			newDiv.style.backgroundColor = "black";
+			newDiv.style.width = "50%";
+			newDiv.style.color = "white";
+			newDiv.style.borderRadius = "25px";
+			newDiv.style.padding = "10px";
+
+			//Ajout des éléments
+			newDiv.appendChild(newCheckBox);
+			newDiv.appendChild(textNode);
+			taskList.appendChild(newDiv);
 		}
 	} catch (error) {
+		console.log("error", taskName.value);
 		alert("error server");
 	}
 });
 
-// newTask.addEventListener("click", () => {
-// 	console.log(newTask.id);
-// });
+newDiv.addEventListener("click", () => {
+	console.log("allo");
+	
+})
