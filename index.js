de00f3d46f88079ba164cef0f288c61f59c50a4e
Kyllian <kyllianvibancos2@gmail.com>
2025-04-09 13:45:46 +0200

    fin

--- index.js
+++ index.js
@@ -2,19 +2,11 @@ let taskName = document.querySelector("#task-name");
 let submit = document.querySelector("#submit");
 let taskList = document.querySelector("#task-list");
 
-let id = 0;
+let id = 1;
 let newTask;
 let taskItems = [];
-console.log(taskName);
 
-//createelement appendchild pour ajouter
-// class TodoList {
-// 	constructor(id, title, taskItems) {
-// 		this.id = id++;
-// 		this.title = title;
-// 		this.taskItems = taskItems;
-// 	}
-// }
+//Création de la classe
 class Todo {
 	constructor(id, title) {
 		this.id = id++;
@@ -31,6 +23,7 @@ class Todo {
 	}
 }
 
+//Fonction d'ajout de todo
 submit.addEventListener("click", (event) => {
 	event.preventDefault();
 	try {
@@ -41,30 +34,58 @@ submit.addEventListener("click", (event) => {
 		) {
 			alert("Please enter a title for your task");
 		} else {
-
 			//Instanciation de la classe et ajout dans le tableau
 			newTask = new Todo(id++, `${taskName.value}`);
 			taskItems.push(newTask);
-			console.log(taskItems);
 
 			//Création d'éléments dynamiques
-			textNode = document.createTextNode(newTask.title);
+			taskText = document.createTextNode(newTask.title);
 			newDiv = document.createElement("div");
 			newCheckBox = document.createElement("input");
 			newCheckBox.type = "checkbox";
 			newCheckBox.id = newTask.id;
 			newCheckBox.value = newTask.completed;
+			deleteButton = document.createElement("button");
+			buttonText = document.createTextNode("Delete");
 
-			//Style des éléments
-			newDiv.style.backgroundColor = "black";
-			newDiv.style.width = "50%";
-			newDiv.style.color = "white";
-			newDiv.style.borderRadius = "25px";
-			newDiv.style.padding = "10px";
+			deleteButton.classList.add("deleteButton");
+			newDiv.classList.add("taskDiv");
+
+			// Fonction pour mettre à jour le style visuel selon l'état
+			function updateTaskStyle(task, div) {
+				if (task.completed) {
+					div.style.textDecoration = "line-through";
+					div.style.opacity = "0.6";
+				} else {
+					div.style.textDecoration = "none";
+					div.style.opacity = "1";
+				}
+			}
+
+			// Événement de clic sur la checkbox : toggle l'état
+			newCheckBox.addEventListener("click", () => {
+				newTask.toggle();
+				updateTaskStyle(newTask, newDiv);
+			});
+
+			deleteButton.addEventListener("click", () => {
+				// Trouver l’index de la tâche dans le tableau
+				const index = taskItems.findIndex(
+					(task) => task.id === newTask.id
+				);
+
+				if (index !== -1) {
+					taskItems.splice(index, 1); // Supprimer la tâche du tableau
+					newDiv.remove(); // Supprimer la tâche de l’interface
+					console.log(`Tâche supprimée : ${newTask.title}`);
+				}
+			});
 
 			//Ajout des éléments
 			newDiv.appendChild(newCheckBox);
-			newDiv.appendChild(textNode);
+			newDiv.appendChild(taskText);
+			newDiv.appendChild(deleteButton);
+			deleteButton.appendChild(buttonText);
 			taskList.appendChild(newDiv);
 		}
 	} catch (error) {
@@ -72,8 +93,3 @@ submit.addEventListener("click", (event) => {
 		alert("error server");
 	}
 });
-
-newDiv.addEventListener("click", () => {
-	console.log("allo");
-	
-})
